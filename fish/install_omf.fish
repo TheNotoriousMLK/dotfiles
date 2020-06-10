dc40c3a99655ae35325e9cfaf0a047d7a1b67cb9
MLK <mlk.cerberus@outlook.com>
2020-06-10 12:40:45 -0500

    Other

--- fish/install_omf.fish
+++ fish/install_omf.fish
@@ -2,7 +2,7 @@
 
 function install_omf
   if ! type omf > /dev/null 2>&1
-    curl -L https://get.oh-my.fish > /dev/null 2>&1 | fish > /dev/null 2>&1
+    curl -L https://get.oh-my.fish | fish
     omf install > /dev/null 2>&1
   end
 end
